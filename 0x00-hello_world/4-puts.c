b38c88233d6367191b156e8dd3b4a31fd0160ff1
michaelteshale <teshalemichael@gmail.com>
2022-04-08 21:28:06 +0000

    test2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*Use the function puts*/
 int main(void)
+  
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
 }
