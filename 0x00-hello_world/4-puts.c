be9d1363df9ace66756e64fe4ace5d9c6563c1e8
michaelteshale <teshalemichael@gmail.com>
2022-04-09 04:35:18 -0700

    tesk 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+
 /**
- *Use the function puts
- *hello world 0x01
+
+ * Description: main - Prints out a string to stdout.
+
+ * Return: 0 if success.
+
  */
+
 int main(void)
+  
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+  
 }
