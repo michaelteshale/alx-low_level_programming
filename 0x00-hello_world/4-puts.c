1f407c59e670c9d207547d3e501a685473c15f41
michaelteshale <teshalemichael@gmail.com>
2022-04-09 04:33:16 -0700

    tesk 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  *Use the function puts
  *hello world 0x01
-*/
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
