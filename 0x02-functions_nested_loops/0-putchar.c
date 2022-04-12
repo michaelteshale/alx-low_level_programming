cec5266ebf719b147928d0ff63dd624bf655f1a3
michaelteshale <teshalemichael@gmail.com>
2022-04-12 06:14:51 -0700

    test1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
-
 /**
-
  *Print _putchar including with newline
-
  * Return: 0 on success.
-
  */
-
 int main(void)
-  
 {
-  
-  char c[] = "_putchar";
-  
-  int i;
-  
-
-  
-  for (i = 0; i < 7; i++)
-    
-    {
-      
-   putchar(c[i]);
-      
-    }
-  
- putchar('\n');
-  
-  return (0);
-  
+char c[] = "_putchar";
+int i;
+for (i = 0; i < 7; i++)
+{
+putchar(c[i]);
+}
+putchar('\n');
+return (0);
 }
