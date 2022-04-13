96655681f1781371703944d455ebb7457e6c7832
michaelteshale <teshalemichael@gmail.com>
2022-04-12 19:00:56 -0700

    task 9

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- *Print _putchar including with newline
- * Return: 0 on success.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char *sh = "Holberton";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
 _putchar('\n');
 return (0);
 }
