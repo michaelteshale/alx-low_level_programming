7db0a9f3217914202edbc3359cf6b725f6c7860f
michaelteshale <teshalemichael@gmail.com>
2022-04-18 14:41:02 -0700

    final taks

---
+++ 101-keygen.c
@@ -0,0 +1,17 @@
+#include <main.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *Task 12
+ *generates random valid passwords for
+ * the program 101-crackme.
+ */
+int main(void)
+{
+int num;
+srand(time(0));
+num =rand();
+printf("%i\n", num);
+return (0);
+}
