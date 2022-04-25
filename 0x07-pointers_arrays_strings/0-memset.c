ea2be125587a4412fafd69d506ba57555a602540
michaelteshale <teshalemichael@gmail.com>
2022-04-25 07:08:29 -0700

    task 0-1 second try commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,23 @@
 /*
- * File: 0-memset.c
+ * File: 1-memcpy.c
  * Auth: Brennan D Baraban
  */
 #include "main.h"
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
+ * _memcpy - Copies @n bytes from the memory area pointed
+ *           to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src.
  *
- * Return: A pointer to the filled memory area @s.
+ * Return: A pointer to the destination buffer @dest.
  */
-void *_memset(void *s, int c, size_t n)
+void *_memcpy(void *dest, const void *src, size_t n)
 {
 unsigned int index;
-unsigned char *memory = s, value = c;
+unsigned char *destination = dest;
+const unsigned char *source = src;
 for (index = 0; index < n; index++)
-memory[index] = value;
-return (memory);
+destination[index] = source[index];
+return (dest);
 }
