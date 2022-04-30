456010e577c5aeadfa5fe5d1085276f51a06a312
michaelteshale <teshalemichael@gmail.com>
2022-04-30 01:27:49 -0700

    task100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,42 +1,61 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 /**
- * subtract_largest_coin - find min number of coins needed to make change
- * @cents: the amount of money to make change for in cents
- *
- * Return: the minimum number of coins needed to make change
+ * coinConverter - Helper function that does all the mathematics
+ * @i: Passed in variable from main for calculations
+ * Return: The number of coins needed minimum for the passed in variable
  */
-int subtract_largest_coin(int cents)
+int coinConverter(int i)
 {
-if (cents - 25 > -1)
-return (cents - 25);
-if (cents - 10 > -1)
-return (cents - 10);
-if (cents - 5 > -1)
-return (cents - 5);
-if (cents - 2 > -1)
-return (cents - 2);
-if (cents - 1 > -1)
-return (cents - 1);
-return (0);
+int count = 0;
+while (i != 0)
+{
+if (i % 10 == 9 || i % 10 == 7)
+i -= 2;
+else if (i % 25 == 0)
+i -= 25;
+else if (i % 10 == 0)
+i -= 10;
+else if (i % 5 == 0)
+i -= 5;
+else if (i % 2 == 0)
+{
+if (i % 10 == 6)
+i -= 1;
+else
+i -= 2;
+}
+else
+i -= 1;
+count++;
+}
+return (count);
 }
 /**
- * main - print the minimum number of coins needed to make change
- * @argc: size of the argument vector
- * @argv: program name and arguments
- *
- * Return: 1 if given something other than a single argument,
- * otherwise 0
+ * main - Takes in exactly one argument for minimum coin count
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise
  */
 int main(int argc, char *argv[])
 {
-int cents, coins;
+int i, coin;
+coin = 0;
 if (argc != 2)
 {
-puts("Error");
+printf("Error\n");
 return (1);
 }
-for (cents = atoi(argv[1]), coins = 0; cents > 0; ++coins)
-cents = subtract_largest_coin(cents);
-printf("%d\n", coins);
+i = atoi(argv[1]);
+if (i < 0)
+printf("0\n");
+else
+{
+coin = coinConverter(i);
+printf("%d\n", coin);
+}
 return (0);
 }
