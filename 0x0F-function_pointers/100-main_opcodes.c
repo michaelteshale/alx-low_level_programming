dd13aebf08da497a64ae5d1644bef0d60c65ae18
michaelteshale <teshalemichael@gmail.com>
2022-05-11 14:48:43 -0700

    main_opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,34 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <udis86.h>
-/**
- * main - ...
- * @argc: ...
- * @argv: ...
- *
- * Return: ...
- */
+#include<stdio.h>
+#include<stdlib.h>
 int main(int argc, char *argv[])
 {
-ud_t ud_obj;
-int val = 0, i = 0;
-if (argc == 2)
+int i, n;
+if (argc != 2)
 {
-val = atoi(argv[1]);
-if (val < 0)
+printf("Error\n");
+return (1);
+}
+n = atoi(argv[1]);
+if (n < 0)
 {
 printf("Error\n");
 exit(2);
 }
-ud_unit(&ud_obj);
-ud_set_input_buffer(&ud_obj, argv[1], val);
-ud_set_mode(&ud_obj, 64);
-ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-while (ud_disassemble(&ud_obj))
+for (i = 0; i < n; i++)
 {
-printf("\t%s\n", ud_insn_hex(&ud_obj));
-}
+printf("%02hhx", *((char *)main + i));
+if (i < n - 1)
+printf(" ");
+else
+printf("\n");
 }
 return (0);
 }
-
